0ca6d1db6b77493d84477f13d156f6f1493b4276
Ashok Koyi <ashok.koyi@gmail.com>
2018-03-11 04:05:53 +0530

    fix(build): Corrected build issues

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,19 @@
 export default {
-  entry: 'dist/index.js',
-  dest: 'dist/bundles/x-ng4-http-interceptor-dontuse.umd.js',
-  sourceMap: false,
-  format: 'umd',
-  moduleName: 'ng.x-ng4-http-interceptor-dontuse',
-  globals: {
-    '@angular/core': 'ng.core',
-    '@angular/http': 'ng.http',
-    'rxjs/Observable': 'Rx',
-    'rxjs/add/observable/empty': 'Rx.Observable',
-    'rxjs/add/observable/of': 'Rx.Observable',
-    'rxjs/add/observable/throw': 'Rx.Observable',
-    'rxjs/add/operator/catch': 'Rx.Observable.prototype',
-    'rxjs/add/operator/mergeMap': 'Rx.Observable.prototype'
+  input: 'dist/index.js',
+  sourcemap: false,
+  output: {
+    name: 'ng.x-ng4-http-interceptor-dontuse',
+    globals: {
+      '@angular/core': 'ng.core',
+      '@angular/http': 'ng.http',
+      'rxjs/Observable': 'Rx',
+      'rxjs/add/observable/empty': 'Rx.Observable',
+      'rxjs/add/observable/of': 'Rx.Observable',
+      'rxjs/add/observable/throw': 'Rx.Observable',
+      'rxjs/add/operator/catch': 'Rx.Observable.prototype',
+      'rxjs/add/operator/mergeMap': 'Rx.Observable.prototype'
+    },
+    format: 'umd',
+    file: 'dist/bundles/x-ng4-http-interceptor-dontuse.umd.js'
   }
-}
+};
--- src/interceptor-service.ts
+++ src/interceptor-service.ts
@@ -17,6 +17,7 @@ import 'rxjs/add/observable/empty';
 import 'rxjs/add/observable/of';
 import 'rxjs/add/observable/throw';
 import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/mergeMap';
 
 import { HttpDirect } from './http-direct';
